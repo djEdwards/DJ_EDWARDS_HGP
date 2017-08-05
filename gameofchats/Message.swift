868bddb1d07226d0d9c163c7868acd2c49a446bc
djEdwards <darrelledwards88@gmail.com>
2017-08-04 18:36:56 -0700

    Add files via upload

---
+++ gameofchats/Message.swift
@@ -0,0 +1,37 @@
+//
+//  Message.swift
+//  youngblackenterprise
+//
+//  Created by Dj Edwards on 7/2/17.
+//  Copyright © 2017 HiddenGeniusProject. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class Message: NSObject {
+
+    var fromId: String?
+    var text: String?
+    var timestamp: NSNumber?
+    var toId: String?
+    var imageUrl: String?
+    var imageWidth: NSNumber?
+    var imageHeight: NSNumber?
+    
+    init(dictionary: [String: Any]) {
+        self.fromId = dictionary["fromId"] as? String
+        self.text = dictionary["text"] as? String
+        self.toId = dictionary["toId"] as? String
+        self.timestamp = dictionary["timestamp"] as? NSNumber
+        self.imageUrl = dictionary["imageUrl"] as? String
+        
+        self.imageWidth = dictionary["imageWidth"] as? NSNumber
+        self.imageHeight = dictionary["imageHeight"] as? NSNumber
+    }
+    
+    func chatPartnerId() -> String? {
+        return fromId == FIRAuth.auth()?.currentUser?.uid ? toId : fromId
+    }
+    
+}
---
+++ gameofchats/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  youngblackenterprise
+//
+//  Created by Dj Edwards on 7/2/17.
+//  Copyright © 2017 HiddenGeniusProject. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var id: String?
+    var name: String?
+    var email: String?
+    var profileImageUrl: String?
+    init(dictionary: [String: AnyObject]) {
+        self.id = dictionary["id"] as? String
+        self.name = dictionary["name"] as? String
+        self.email = dictionary["email"] as? String
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String
+    }
+}
